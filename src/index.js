c40d978ccc8f741018ee2972ba1a4d161db48f1a
olga_mirocz <ol.miroch@gmail.com>
2021-10-27 00:05:02 +0300

    add get methods for all users and schedules

--- src/index.js
+++ src/index.js
@@ -1,18 +1,39 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+
+const { users } = require('./data');
+const { schedules } = require('./data');
+
 
 app.get('/', (req, res) => {
-  res.status(200).send('Welcome to our schedule website!');
+  res
+  .status(404)
+  .send('Welcome to our schedule website!');
 });
 
 app.get('/users', (req, res) => {
-  res.send('');
+  res
+  .status(200).json({
+    status: 'success',
+    results: users.length,
+    data: {
+      users
+    }
+  });
 });
 
-app.get('schedules', (req, res) => {
-  res.send('');
+app.get('/schedules', (req, res) => {
+  res
+  .status(200).json({
+    status: 'success',
+    results: schedules.length,
+    data: {
+      schedules
+    }
+  });
 });
+
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log('Up & Running...');
