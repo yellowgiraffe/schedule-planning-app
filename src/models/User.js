3aa9e417424e43c6af97658a2256ed923438b24c
olga_mirocz <ol.miroch@gmail.com>
2021-11-09 23:29:34 +0300

    add image url to user class

--- src/models/User.js
+++ src/models/User.js
@@ -1,11 +1,12 @@
 const { users } = require('../data');
 
 module.exports = class User {
-  constructor(firstname, lastname, email, passowrd) {
+  constructor(firstname, lastname, email, passowrd, imageUrl) {
     this.firstname = firstname;
     this.lastname = lastname;
     this.email = email;
     this.passowrd = passowrd;
+    this.imageUrl = imageUrl;
   }
 
   add() {
